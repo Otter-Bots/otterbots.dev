e338f31b387ffa81080d7a34b7d408c441ea0e8d
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-02-10 22:06:32 +0000

    begin

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'theme-ui'
+import theme from '@hackclub/theme'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    // @ts-expect-error
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
